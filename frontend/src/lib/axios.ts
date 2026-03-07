8900f8e33586047174421f2b1a80a492656047b6
AbhishekSinhaa17 <abhiks1710@gmail.com>
2026-03-07 17:51:50 +0530

    updated api url

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -2,5 +2,5 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:4000/api" : "/api", // ✅ This must match your backend
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:4000/api" : "https://tunex.onrender.com/api", // ✅ This must match your backend
 });
